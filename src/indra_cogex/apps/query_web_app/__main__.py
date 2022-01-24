6f4d20ee05d5fbe423a97352fd8a98913e3ab95a
kkaris <karis.klas@gmail.com>
2022-01-24 11:03:12 -0500

    WIP import app in main

--- src/indra_cogex/apps/query_web_app/__main__.py
+++ src/indra_cogex/apps/query_web_app/__main__.py
@@ -2,7 +2,7 @@
 
 """Run the query web app with ``python -m indra_cogex.apps.query_web_app``."""
 
-from . import cli
-
-if __name__ == '__main__':
-    cli()
+from . import app
+#
+# if __name__ == '__main__':
+#     cli()
